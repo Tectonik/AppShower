97b910c6367220ad91b983c57598e75ddc608be2
Martin Ali <marto7395@gmail.com>
2016-02-06 20:18:01 +0200

    Basic database built

--- appshower-server/data/data.js
+++ appshower-server/data/data.js
@@ -1,36 +1,40 @@
-(function() {
-	'use strict';
+(function () {
+    'use strict';
 
-	const constants = require('./../helpers/constants');
-	let Everlive = require('everlive-sdk'),
-		el = new Everlive('4l7wacjxymhqahsg');
-	//let myQuery = new everlive.Query();
-	//myQuery.where().isin('Name', ['Doncho', 'Evlogi']);
+    const constants = require('./../helpers/constants');
+    let Everlive = require('everlive-sdk'),
+        el = new Everlive({
+            appId: '4l7wacjxymhqahsg',
+            scheme: 'http'
+        });
 
-	// let telerikCourseData = el.data('TelerikCourse');
+    //let myQuery = new everlive.Query();
+    //myQuery.where().isin('Name', ['Doncho', 'Evlogi']);
 
-	function getAllWithQuery(typeData, query) {
-		return new Promise((resolve, reject) => {
-			typeData
-				.get(query)
-				.then(function(response) {
-						resolve(response.result);
-					},
-					function(error) {
-						reject(error);
-					});
-		});
-	}
+    // let telerikCourseData = el.data('TelerikCourse');
 
-	module.exports = {
-		initialize: function() {
-			return el;
-		},
-		createQuery: function() {
-			return new Everlive.Query();
-		},
-		data: el.data,
-		getAllWithQuery: getAllWithQuery
-	};
+    function getAllWithQuery(typeData, query) {
+        return new Promise((resolve, reject) => {
+            el.data(typeData)
+                .get(query)
+                .then(function (response) {
+                    resolve(response.result);
+                },
+                    function (error) {
+                        reject(error);
+                    });
+        });
+    }
 
-}());
+    module.exports = {
+        initialize: function () {
+            return el;
+        },
+        createQuery: function () {
+            return new Everlive.Query();
+        },
+        data: el.data,
+        getAllWithQuery: getAllWithQuery
+    };
+
+} ());
