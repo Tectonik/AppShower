d5335f53ba0c265e3f63dd3791f483fcc102b8be
Martin Ali <marto7395@gmail.com>
2016-02-06 12:16:20 +0200

    Fixed not installed dependency module "async"

--- appshower-server/routers/index.js
+++ appshower-server/routers/index.js
@@ -1,8 +1,12 @@
 (function () {
     'use strict';
+    console.log("Routers loading...")
 
     const constants = require('./../helpers/constants.js');
     let fileSystem = require('fs');
+    
+    let files = fileSystem
+            .readdirSync(constants.ROUTERS_LOCATION);
 
     module.exports = function (app) {
         fileSystem
