aebbb35837ee9efe81d31f7b0d19cf25b9b4c86b
Martin Ali <marto7395@gmail.com>
2016-02-08 00:08:31 +0200

    Small changes

--- appshower-server/routers/accounts-router.js
+++ appshower-server/routers/accounts-router.js
@@ -17,74 +17,84 @@
                 password = user.Password,
                 username = user.Username;
 
-            db.authentication.login(username, password, function (dataObj) {
-                response
-                    .status(201)
-                    .json({
-                        result: "User logged in successfully",
-                        tokenInfo: dataObj
-                    });
-            }, function (error) {
-                response
-                    .status(404)
-                    .json({
-                        result: "User has not been logged in ",
-                        error: error.message
-                    });
+            db
+                .authentication
+                .login(username, password,
+                    function (dataObj) {
+                        response
+                            .status(201)
+                            .json({
+                                result: "User logged in successfully",
+                                tokenInfo: dataObj
+                            });
+                    },
+                    function (error) {
+                        response
+                            .status(404)
+                            .json({
+                                result: "User has not been logged in ",
+                                error: error.message
+                            });
 
-                console.log(error);
-            });
+                        console.log(error);
+                    });
         })
-        .put('/logout', function (request, response) {
-            db.authentication.logout(function () {
-                response
-                    .status(201)
-                    .json({
-                        result: "User logged out successfully"
-                    });
-            }, function (error) {
-                response
-                    .status(404)
-                    .json({
-                        result: "User has not been logged out ",
-                        error: error.message
-                    });
+        .put('/logout',
+            function (request, response) {
+                db
+                    .authentication
+                    .logout(
+                        function () {
+                            response
+                                .status(201)
+                                .json({
+                                    result: "User logged out successfully"
+                                });
+                        },
+                        function (error) {
+                            response
+                                .status(404)
+                                .json({
+                                    result: "User has not been logged out ",
+                                    error: error.message
+                                });
 
-                console.log(error);
+                            console.log(error);
+                        });
+            })
+        .post('/register',
+            function (request, response) {
+                let newUser = request.body,
+                    password = newUser.Password,
+                    username = newUser.Username,
+                    otherAttributes = {
+                        Email: newUser.Email,
+                        DisplayName: newUser.DisplayName,
+                        CreatedAt: new Date(),
+                        ModifiedAt: new Date(),
+                        Courses: newUser.Courses,
+                        Image: newUser.Image
+                    };
+
+                db.Users.register(username, password, otherAttributes,
+                    function (dataObj) {
+                        response
+                            .status(201)
+                            .json({
+                                result: "User created successfully"
+                            });
+                    },
+                    function (error) {
+                        response
+                            .status(404)
+                            .json({
+                                result: "User has not been created",
+                                error: error.message
+                            });
+
+                        console.log(error);
+                    });
             });
-        })
-        .post('/register', function (request, response) {
-            let newUser = request.body,
-                password = newUser.Password,
-                username = newUser.Username,
-                otherAttributes = {
-                    Email: newUser.Email,
-                    DisplayName: newUser.DisplayName,
-                    CreatedAt: new Date(),
-                    ModifiedAt: new Date(),
-                    Courses: newUser.Courses,
-                    Image: newUser.Image
-                };
-
-            db.Users.register(username, password, otherAttributes,
-                function (dataObj) {
-                    response
-                        .status(201)
-                        .json({
-                            result: "User created successfully"
-                        });
-                },
-                function (error) {
-                    response
-                        .status(404)
-                        .json({
-                            result: "User has not been created",
-                            error: error.message
-                        });
-
-                    console.log(error);
-                });
-        });
 
     module.exports = {
         controller: {},
